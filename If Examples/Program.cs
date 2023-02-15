fe0e26f1b901e871d074b783bf4244ab4b37d967
hopefullyicanchangethislater <121258893+hopefullyicanchangethislater@users.noreply.github.com>
2023-02-15 14:32:28 -0500

    Added the double bonus.

--- If Examples/Program.cs
+++ If Examples/Program.cs
@@ -1,20 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
+﻿Random dice = new Random();
 
-Random dice = new Random();
-
-int roll1 = dice.Next(1,7);
+int roll1 = dice.Next(1, 7);
 int roll2 = dice.Next(1, 7);
 int roll3 = dice.Next(1, 7);
 
-int total = roll1+ roll2 + roll3;
+int total = roll1 + roll2 + roll3;
 
 Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
-if (total > 14)
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
 {
-    Console.WriteLine("You Win!");
+    Console.WriteLine("You rolled doubles! +2 bonus to total!");
+    total += 2;
 }
+
+if (total >= 15)
+{
+    Console.WriteLine("You win!");
+}
+
 if (total < 15)
 {
     Console.WriteLine("Sorry, you lose.");
