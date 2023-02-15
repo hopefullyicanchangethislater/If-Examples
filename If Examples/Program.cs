524d68daf2574066a25c7d48725a10584043fb90
hopefullyicanchangethislater <121258893+hopefullyicanchangethislater@users.noreply.github.com>
2023-02-15 14:52:44 -0500

    Could still use a place to store and pull results. Needs a lot more functionality, even if just for me.

--- If Examples/Program.cs
+++ If Examples/Program.cs
@@ -1,31 +1,48 @@
-﻿Random dice = new Random();
+﻿// Could still use a place to store and pull results. Needs a lot more functionality, even if just for me. 
 
+
+// initialize the dice object using the Random() method.
+Random dice = new Random();
+
+//random roles between 1 and 6
 int roll1 = dice.Next(1, 7);
 int roll2 = dice.Next(1, 7);
 int roll3 = dice.Next(1, 7);
 
+// add 'em up
 int total = roll1 + roll2 + roll3;
 
+// Display what was rolled and the total
 Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
+// This is the bonus logic.
 if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
 {
-    Console.WriteLine("You rolled doubles! +2 bonus to total!");
-    total += 2;
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+        total += 6;
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+        total += 2;
+    }
 }
-
-if ((roll1 == roll2) && (roll2 == roll3))
+// This gives the prize output at the end
+if (total >= 16)
 {
-    Console.WriteLine("You rolled triples! +6 bonus to total!");
-    total += 6;
+    Console.WriteLine("You win a new car!");
 }
-
-if (total >= 15)
+else if (total >= 10)
 {
-    Console.WriteLine("You win!");
+    Console.WriteLine("You win a new laptop!");
 }
-
-if (total < 15)
+else if (total == 7)
 {
-    Console.WriteLine("Sorry, you lose.");
+    Console.WriteLine("You win a trip for two!");
+}
+else
+{
+    Console.WriteLine("You win a kitten!");
 }
